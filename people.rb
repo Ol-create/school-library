aaf8887416c5acde22d42a2c6140e5289630d59d
Oluyemi Paul <oluola96@gmail,com>
2022-11-03 11:18:12 +0100

    add is_of_age method

--- people.rb
+++ people.rb
@@ -16,7 +16,16 @@ class Person
         @name,
         @age
     end
+
+    #Create update_info method
     def update_info = ( name, age )
         @name = name
         @age = age
+
+    #Create private is_of_age? method
+    def is_of_age?(age)
+        return (age >= 18)
+    end
+
+    private :is_of_age?
 end
