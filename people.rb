735633ef3c56f19ab7465be5046104c10b3c3449
Oluyemi Paul <oluola96@gmail,com>
2022-11-03 11:01:26 +0100

    create constuctor class

--- people.rb
+++ people.rb
@@ -2,4 +2,11 @@ class Person
     @id, 
     @name,
     @age
+
+    #Create constructor
+    def initialize ( age, name ="unknown", parent_permission = true)
+        @age = age
+        @name = name
+        @parent_permission = parent_permission
+    end
 end
