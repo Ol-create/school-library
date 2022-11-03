b24673b58b33c83ba4b3fcdb950d708ab63e7b14
Oluyemi Paul <oluola96@gmail,com>
2022-11-03 11:45:29 +0100

    Override can_use_service? to true

--- people.rb
+++ people.rb
@@ -58,4 +58,9 @@ class Teacher < Person
     def initialize( specialization )
         @specialization = specialization
     end
+
+    #Create can_use_services? method to override can_use_services in Person class
+    def can_use_services?
+        return true
+    end
 end
