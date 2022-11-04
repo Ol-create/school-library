4897bc2bc1aa740188b750eb3b45251a7f63dff1
Oluyemi Paul <oluola96@gmail,com>
2022-11-04 09:28:01 +0100

    resolved

--- person.rb
+++ person.rb
@@ -1,22 +1,43 @@
-class Person < Nameable
-  attr_reader :id
-  attr_accessor :name, :age
+require 'SecureRandom'
+require './nameable'
+require './capitalize_decorator'
+require './trimmed_decorator'
 
-  def initialize(age, name = 'unknown', parent_permission: true)
+class Person < Nameable
+  attr_accessor :name, :age, :parent_permission
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
-    @id = Random.rand(1..10_000)
+    @id = SecureRandom.uuid
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def can_use_services?
-    of_age? || parent_permission
-  end
-
-  private
-
   def of_age?
     @age >= 18
   end
+
+  private :of_age?
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
+  end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.name
+# => maximilianus
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+# => Maximilianus
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
+# => Maximilian
