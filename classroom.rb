989e72e795f73ace534248542ae1318704e47404
Oluyemi Paul <oluola96@gmail,com>
2022-11-04 10:57:44 +0100

    add classroom class

---
+++ classroom.rb
@@ -0,0 +1,17 @@
+class Classroom
+  attr_accessor :label
+  attr_reader :students
+
+  #Create Constructor 
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  #Add student to class
+  def add_student(student)
+    @students << student unless students.include?(student)
+    student.classroom = self
+  end
+
+end
